11b478798811e132b705fdefc522678b13787c3b
alby2255 <alby7370@gmail.com>
2024-11-30 18:11:23 -0500

    updated readme file

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_packages
 
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="moviequote_generator",  # Replace with your library name
-    version="0.1.0",
+    version="0.2.0",
     author="Albin Biju",
     author_email="alby7370@gmail.com",
-    description="This is random movie quote generator",
-    long_description=open("README.md").read(),
+    description="This is a movie quote generator",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/alby2255/moviequote_generator",
     packages=find_packages(),
